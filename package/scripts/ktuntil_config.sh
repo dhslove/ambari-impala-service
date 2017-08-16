b93578050f9d5031b57ee0d2fd958504d6bbba55
shishhx <740240634@qq.com>
2017-08-16 14:19:55 +0800

    mpack方式管理
    在ambari2.4.1-HDP2.5、ambari2.5.1-HDP2.6环境下测试，测试中启用了Kerberos认证

--- package/scripts/ktuntil_config.sh
+++
@@ -1,8 +0,0 @@
-ktutil << EOF
-rkt /etc/security/keytabs/impala.service.keytab
-rkt /etc/security/keytabs/impala-http.service.keytab
-wkt /etc/security/keytabs/impala-http.keytab
-quit
-EOF
-chmod 400 /etc/security/keytabs/impala-http.keytab
-chown impala:impala /etc/security/keytabs/impala-http.keytab
--- package/scripts/params.py
+++ package/scripts/params.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 from resource_management import *
 from resource_management.libraries.script.script import Script
-import sys, os, glob ,socket
+import os, socket
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+files_dir = os.path.join(os.path.dirname(script_dir), 'files')
 
 # server configurations
 config = Script.get_config()
+tmp_dir = Script.get_tmp_dir()
+stack_root = Script.get_stack_root()
+stack_name = default("/hostLevelParams/stack_name", None)
+stack_version_buildnum = default("/commandParams/version", None)
 
 impala_user = config['configurations']['impala-env']['impala_user']
 impala_group = config['configurations']['impala-env']['impala_group']
@@ -15,7 +22,22 @@ impala_log_file = os.path.join(impala_log_dir,'impala-setup.log')
 impala_catalog_host = config['clusterHostInfo']['impala_catalog_service_hosts'][0]
 impala_state_store_host = config['clusterHostInfo']['impala_state_store_hosts'][0]
 
-service_packagedir = os.path.realpath(__file__).split('/scripts')[0]
 current_host_name = socket.gethostname()
 
 security_enabled = config['configurations']['cluster-env']['security_enabled']
+
+hdfs_host = default("/clusterHostInfo/namenode_hosts", [''])[0]
+if not hdfs_host:
+    hdfs_host = default("/clusterHostInfo/namenode_host", [''])[0]
+hive_host = default("/clusterHostInfo/hive_metastore_host", [''])[0]
+scp_conf_dir = "/etc/impala/conf"
+scp_conf_from = {
+    "hive": {
+        "host": hive_host,
+        "files": ["/etc/hive/conf/hive-site.xml"]},
+    "hdfs": {
+        "host": hdfs_host,
+        "files": [
+            "/etc/hadoop/conf/core-site.xml",
+            "/etc/hadoop/conf/hdfs-site.xml"]},
+}
